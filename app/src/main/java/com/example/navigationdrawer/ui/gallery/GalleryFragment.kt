066a4218a358b598467f922f55f0567b1b48c81c
Joseph <21100565@ue.edu.pe>
2024-05-12 13:56:17 -0500

    Login and Web

--- app/src/main/java/com/example/navigationdrawer/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/gallery/GalleryFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.navigationdrawer.databinding.FragmentGalleryBinding
+import com.example.navigationdrawer.ui.shared.HomeGalleryViewModel
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
+    private val homeGalleryViewModel: HomeGalleryViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,13 +33,25 @@ class GalleryFragment : Fragment() {
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val wuPage: WebView = binding.wuPage
+        wuPage.settings.javaScriptEnabled = true
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        wuPage.webViewClient = object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean{
+                if(url!=null)
+                    view?.loadUrl(url)
+                return true
+            }
         }
+
+        homeGalleryViewModel.url.observe(viewLifecycleOwner){url->
+            wuPage.loadUrl(url)
+        }
+
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
